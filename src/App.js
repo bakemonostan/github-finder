78930592070a5d6c482d5934c01994a349213294
bakemonostan <ehizomokhomion@gmail.com>
2022-01-04 07:54:04 -0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Footer from './components/layout/Footer';
+
+//Components
+import Navbar from './components/layout/Navbar';
+import About from './Pages/About';
+import Home from './Pages/Home';
+import NotFound from './Pages/NotFound';
+
 export default function App() {
   return (
-    <div className='bg-purple-500'>
-      <h1>Hello world</h1>
-    </div>
+    <Router>
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar />
+        <main className='conatiner mx-auto px-3'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/404' element={<NotFound />} />
+            {/* catch all routes that wld send user to a 404 page */}
+            <Route path='/*' element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+// import UserResults from '../components/users/UserResults';
+// import UserSearch from '../components/users/UserSearch';
+
+import UserResults from '../components/Users/UserResults';
+
+function Home() {
+  return (
+    <>
+      {/* Search Component */}
+      <UserResults />
+    </>
+  );
+}
+
+export default Home;
