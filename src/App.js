2513da56d7fc103293379c50d0761f97d8baa10c
bakemonostan <ehizomokhomion@gmail.com>
2022-03-17 00:51:42 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -6,23 +6,26 @@ import Navbar from './components/layout/Navbar';
 import About from './Pages/About';
 import Home from './Pages/Home';
 import NotFound from './Pages/NotFound';
+import { GithubProvider } from './context/github/GithubContext';
 
 export default function App() {
   return (
-    <Router>
-      <div className='flex flex-col justify-between h-screen'>
-        <Navbar />
-        <main className='conatiner mx-auto px-3'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/404' element={<NotFound />} />
-            {/* catch all routes that wld send user to a 404 page */}
-            <Route path='/*' element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <GithubProvider>
+      <Router>
+        <div className='flex flex-col justify-between h-screen'>
+          <Navbar />
+          <main className='conatiner mx-3 px-3'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/404' element={<NotFound />} />
+              {/* catch all non-existnt routes that wld send user to a 404 page */}
+              <Route path='/*' element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   );
 }
--- src/components/Users/UserResults.jsx
+++ src/components/Users/UserResults.jsx
@@ -1,5 +1,26 @@
+import { useEffect, useContext } from 'react';
+import GithubContext from '../../context/github/GithubContext';
+import Spinner from '../layout/Spinner';
+import UserItem from './UserItem';
+
 function UserResults() {
-  return <div>User Results</div>;
+  const { users, isloading, fetchUsers } = useContext(GithubContext);
+
+  useEffect(() => {
+    fetchUsers();
+  }, [fetchUsers]); 
+
+  if (!isloading) {
+    return (
+      <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
 }
 
 export default UserResults;
